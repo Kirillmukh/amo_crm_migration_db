961c4dc6387cb9ab5a1e66984887d349ba996e61
Kirillmukh <Kirillmukh@mail.ru>
2024-10-01 00:56:37 +0300

    add hateoas to every choices

--- src/main/java/com/example/dbeaver/controller/MainController.java
+++ src/main/java/com/example/dbeaver/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.example.dbeaver.controller;
 
+import com.example.dbeaver.dto.response.ResponseCompanyDTO;
+import com.example.dbeaver.dto.response.ResponseContactDTO;
+import com.example.dbeaver.dto.response.ResponseLeadDTO;
 import com.example.dbeaver.dto.response.ResponseOpportunityDTO;
 import com.example.dbeaver.exception.WrongIdException;
 import com.example.dbeaver.facade.Facade;
@@ -17,14 +20,20 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 @RequiredArgsConstructor
 public class MainController {
-        private final Facade facade;
+    private final Facade facade;
+    private final Class<MainController> controllerClass = MainController.class;
     @GetMapping("/lead/{id}")
     public Response leadById(@PathVariable String id) {
         return Response.ok(facade.findLeadById(id)).status(200).build();
     }
     @GetMapping("/lead")
-    public Response leads() {
-        return Response.ok(facade.findLeads()).status(200).build();
+    public Response leads(@RequestParam(name = "limit", defaultValue = "500", required = false) int limit,
+                          @RequestParam(name = "offset", defaultValue = "0", required = false) int offset) {
+        List<ResponseLeadDTO> result = facade.findLeads(limit, offset);
+        CollectionModel<ResponseLeadDTO> model = CollectionModel.of(result,
+                linkTo(methodOn(controllerClass).leads(limit, Math.max(0, offset - limit))).withRel("prevOrFirst"),
+                linkTo(methodOn(controllerClass).leads(limit, offset + limit)).withRel("next"));
+        return Response.ok(model).status(200).build();
     }
 
     @GetMapping("/company/{id}")
@@ -32,16 +41,26 @@ public class MainController {
         return Response.ok(facade.findCompanyById(id)).status(200).build();
     }
     @GetMapping("/company")
-    public Response companies() {
-        return Response.ok(facade.findCompanies()).status(200).build();
+    public Response companies(@RequestParam(name = "limit", defaultValue = "500", required = false) int limit,
+                              @RequestParam(name = "offset", defaultValue = "0", required = false) int offset) {
+        List<ResponseCompanyDTO> result = facade.findCompanies(limit, offset);
+        CollectionModel<ResponseCompanyDTO> model = CollectionModel.of(result,
+                linkTo(methodOn(controllerClass).companies(limit, Math.max(0, offset - limit))).withRel("prevOrFirst"),
+                linkTo(methodOn(controllerClass).companies(limit, offset + limit)).withRel("next"));
+        return Response.ok(model).status(200).build();
     }
     @GetMapping("/contact/{id}")
     public Response contactById(@PathVariable String id) {
         return Response.ok(facade.findContactById(id)).status(200).build();
     }
     @GetMapping("/contact")
-    public Response contacts() {
-        return Response.ok(facade.findContacts()).status(200).build();
+    public Response contacts(@RequestParam(name = "limit", defaultValue = "500", required = false) int limit,
+                             @RequestParam(name = "offset", defaultValue = "0", required = false) int offset) {
+        List<ResponseContactDTO> result = facade.findContacts(limit, offset);
+        CollectionModel<ResponseContactDTO> model = CollectionModel.of(result,
+                linkTo(methodOn(controllerClass).contacts(limit, Math.max(0, offset - limit))).withRel("prevOrFirst"),
+                linkTo(methodOn(controllerClass).contacts(limit, offset + limit)).withRel("next"));
+        return Response.ok(model).status(200).build();
     }
     @GetMapping("/opportunity/{id}")
     public Response opportunityById(@PathVariable String id) {
@@ -51,16 +70,15 @@ public class MainController {
     public Response opportunities(@RequestParam(name = "limit", defaultValue = "500", required = false) int limit,
                                   @RequestParam(name = "offset", defaultValue = "0", required = false) int offset) {
         List<ResponseOpportunityDTO> result = facade.findOpportunities(limit, offset);
-
         CollectionModel<ResponseOpportunityDTO> model = CollectionModel.of(result,
-                linkTo(methodOn(MainController.class).opportunities(limit, Math.max(0, offset - 1))).withRel("prevOrFirst"),
-                linkTo(methodOn(MainController.class).opportunities(limit, offset + 1)).withRel("next"));
+                linkTo(methodOn(controllerClass).opportunities(limit, Math.max(0, offset - limit))).withRel("prevOrFirst"),
+                linkTo(methodOn(controllerClass).opportunities(limit, offset + limit)).withRel("next"));
         return Response.ok(model).status(200).build();
     }
-    @GetMapping("/entity")
-    public ResponseEntity<List<ResponseOpportunityDTO>> findOpportunityEntities() {
-        return ResponseEntity.ok(facade.findOpportunities());
-    }
+//    @GetMapping("/entity")
+//    public ResponseEntity<List<ResponseOpportunityDTO>> findOpportunityEntities() {
+//        return ResponseEntity.ok(facade.findOpportunities());
+//    }
 
     @ExceptionHandler(WrongIdException.class)
     public Response handleWrongIdException(WrongIdException exception) {
